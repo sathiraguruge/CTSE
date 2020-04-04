97f8f3b34b8bed567ff8371dfc333591abfff2c5
sathiraguruge <33770377+sathiraguruge@users.noreply.github.com>
2020-04-04 11:02:22 +0530

    Improved Help Screen

--- igamer/igamer/lib/screens/help.dart
+++ igamer/igamer/lib/screens/help.dart
@@ -27,22 +27,70 @@ class HelpScreenPage extends StatelessWidget {
                       Colors.black.withOpacity(0.1), BlendMode.softLight)),
             ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                alignment: Alignment.center,
-                child: Text(
-                  "How to use the app",
-                  style: TextStyle(fontFamily: 'SanFrancisco'),
+          SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.all(10),
+                  child: Text(
+                    "How to use the app",
+                    style: TextStyle(fontFamily: 'SanFrancisco', fontSize: 25),
+                  ),
                 ),
-              )
-            ],
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.only(left: 10),
+                  child: Text(
+                    "App Basics",
+                    style: TextStyle(fontFamily: 'SanFrancisco', fontSize: 20),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    'assets/images/help_one.png',
+                    width: 400,
+                    height: 900,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.only(left: 10),
+                  child: Text(
+                    "Manage Game Reviews",
+                    style: TextStyle(fontFamily: 'SanFrancisco', fontSize: 20),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    'assets/images/help_two.png',
+                    width: 400,
+                    height: 900,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.only(left: 10, bottom: 10),
+                  child: Text(
+                    "That's All !",
+                    style: TextStyle(fontFamily: 'SanFrancisco', fontSize: 40),
+                  ),
+                ),
+              ],
+            ),
           )
         ],
       ),
-      drawer: new CustomizedDrawer(context).getDrawer(), // getting Custom Built Drawer
-      appBar: new CustomizedAppBar(pageTitle).getAppBar(), // getting Custom Built App Bar
+      drawer: new CustomizedDrawer(context).getDrawer(),
+      // getting Custom Built Drawer
+      appBar: new CustomizedAppBar(pageTitle)
+          .getAppBar(), // getting Custom Built App Bar
     );
   }
 }
